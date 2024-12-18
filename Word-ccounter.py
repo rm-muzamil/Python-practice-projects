3b7193816bc97f9df31f757634415b111daf425d
RM Muzammil <ramuzamil44@gmail.com>
2024-12-18 22:59:59 +0500

    word-counter cli-based

---
+++ Word-ccounter.py
@@ -0,0 +1,40 @@
+# def wordCounter(str):
+#     words = str.split()
+#     word_count = {}
+#     for word in words:
+#         word = word.lower()
+#         word = word.strip(".,?!")
+#         if word in word_count:
+#             word_count[word] += 1
+#         else:
+#             word_count[word] = 1
+#     return word_count
+# str = "hello, hello my name is muzammil and people called me rm muzammil but name doesn't matter the thing that matter is behavior"
+# result = wordCounter(str)
+# print(result)    
+
+import tkinter as tk
+
+def wordCounter():
+    str = sentenceEntry.get()
+    words = str.split()
+    word_count = {}
+    for word in words:
+        word = word.lower()
+        word = word.strip(".,?!")
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+app = tk.Tk()
+app.title = "word counter"
+
+sentenceEntry = tk.Entry(app,width=120)
+sentenceEntry.pack(pady=5)
+
+add_button = tk.Button(app, text="Add Task", width=15, command=wordCounter)
+add_button.pack(pady=5)
+
+app.mainloop()
